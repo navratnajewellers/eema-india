8cef289e523c25fe5033ba38a373d918b3b3eac5
Navratna Jewellers <navratnajewellers0@gmail.com>
2025-08-11 11:52:40 +0530

    remove logo from left side to home text

--- src/app/components/WHeader.js
+++ src/app/components/WHeader.js
@@ -8,14 +8,14 @@ export default function WHeader({ headerNavItems, headerImage }) {
   return (
     <header className=" h-full w-full bg-gray-50 relative shadow-md border-b-2 border-gray-200 pb-24  ">
       <section className=" h-[14vh] md:h-[8vh] xl:h-[5vh] w-full">
-        {/* <Link
+        <Link
           href="/"
           className=" h-home-container inline-block fixed top-2 left-2 md:top-11 md:left-14 z-50 py-9 px-7 bg-gray-50 rounded-4xl text-black "
         >
           Home
-        </Link> */}
+        </Link>
 
-        <Link
+        {/* <Link
           href="/"
           className=" h-home-container inline-block fixed top-2 left-2 md:top-11 md:left-14 z-50 py-5 px-4 bg-gray-50 rounded-3xl text-black "
         >
@@ -26,7 +26,7 @@ export default function WHeader({ headerNavItems, headerImage }) {
               fill
             />
           </div>
-        </Link>
+        </Link> */}
 
         <HeaderSideBar />
       </section>
